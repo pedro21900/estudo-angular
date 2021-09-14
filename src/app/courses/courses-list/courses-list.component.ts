182de4326f872f7eed4737f8addba6018a45bd42
Lenonn <lenonn@pop-os.localdomain>
2021-09-14 14:30:51 -0300

    Feat: class COurse

--- src/app/courses/courses-list/courses-list.component.ts
+++ src/app/courses/courses-list/courses-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
   selector: 'app-courses-list',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent implements OnInit {
-
-  constructor() { }
+  courses: Course[] =  [];
 
   ngOnInit(): void {
+    this.courses =[
+    {
+    id: 1,
+    name: "Java: class",
+    imageUrl: "",
+    price: 36.000,
+    code: "XPS-8796",
+    duration: 120,
+    rating: 5.4,
+    releaseDate: "November -13 -2021"
+    },
+    {
+      id: 2,
+      name: "Java: class",
+      imageUrl: "",
+      price: 36.000,
+      code: "LPS-8796",
+      duration: 80,
+      rating: 3.5,
+      releaseDate: "November -29 -2021"
+      }
+    ]
   }
 
 }
